cd851517bffab07c1eab169676872695d256cb89
Marc Knaup <marc@knaup.io>
2019-09-25 21:07:30 +0800

    Dependency hell…

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import com.github.fluidsonic.fluid.library.*
+import org.jetbrains.kotlin.gradle.plugin.*
 
 plugins {
-	id("com.github.fluidsonic.fluid-library") version "0.9.24"
+	id("com.github.fluidsonic.fluid-library") version "0.9.25"
 }
 
 fluidJvmLibrary {
@@ -15,7 +16,13 @@ fluidJvmLibraryVariant {
 }
 
 dependencies {
-	api(fluid("stdlib", "0.9.25"))
+	api(fluid("stdlib", "0.9.25")) {
+		attributes {
+			attribute(KotlinPlatformType.attribute, KotlinPlatformType.jvm)
+			attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, Usage.JAVA_RUNTIME))
+			attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 8)
+		}
+	}
 
 	api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.50")
 	api("org.jetbrains.kotlin:kotlin-annotation-processing-embeddable:1.3.50")
